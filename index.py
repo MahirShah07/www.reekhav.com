af7f578e23852bf308e1eb18f34ef0cd8dde449c
Mahir Shah <mahir.shah.sd@gmail.com>
2023-10-28 11:58:43 +0530

    Update index.py

--- index.py
+++ index.py
@@ -34,4 +34,5 @@ def Gallery():
 def PrivacyPolicy():
     return render_template('privacy_policy.html')
 
-app.run(port=2001)
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
